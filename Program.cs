c3987cda66846f4852a5035d0bc7ffc13e372c50
Alexander <myasoedovuk@gmail.com>
2024-02-29 05:06:32 +0300

    добавил цикл для формирования итоговогомассива

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿string[]array={"hello","244 ","33  ","bobro","333 ","2255","4444","   2"};
 int i= 0;
 int count =0;
+while(i<array.Length){
+    if(array[i].Length<4){count++;}
+    i++;}
+i=0;
+string[]finalarray=new string[count+1];
+count=0;
+while(i<array.Length){
+    if(array[i].Length<4){
+        finalarray[count]=array[i];
+        count++;}
+    i++;}
